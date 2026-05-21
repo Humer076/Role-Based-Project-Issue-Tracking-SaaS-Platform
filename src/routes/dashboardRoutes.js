0a9060fbeacf732c197f3a545925e708349c8949
Humer ali <mdhumerali56@gmail.com>
2026-05-21 14:46:03 +0530

    Update dashboardRoutes.js

--- src/routes/dashboardRoutes.js
+++ src/routes/dashboardRoutes.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
-
-const { getDashboardStats } = require("../controllers/dashboardController");
+const { getDashboardStats, getRecentTasks } = require("../controllers/dashboardController");
 const { verifyToken } = require("../middlewares/auth.middleware");
 
 router.get("/stats", verifyToken, getDashboardStats);
+router.get("/recent", verifyToken, getRecentTasks);
 
 module.exports = router;
